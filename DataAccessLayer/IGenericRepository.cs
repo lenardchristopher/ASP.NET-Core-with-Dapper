500d4d1ddc64cb059c3868449624315f1b4e5087
Christopher Lenard <lenardchristopher@gmail.com>
2019-06-01 15:38:43 -0500

    Include GetConnectionString in IGenericRepo

--- DataAccessLayer/IGenericRepository.cs
+++ DataAccessLayer/IGenericRepository.cs
@@ -1,11 +1,13 @@
 ﻿using Dapper;
 using System.Collections.Generic;
+using System.Data;
 using System.Threading.Tasks;
 
 namespace DataAccessLayer
 {
     public interface IGenericRepository<TEntity>
     {
+        IDbConnection GetOpenConnection();
         Task<IEnumerable<TEntity>> GetAllAsync();
         Task<TEntity> FindAsync(int id);
         void InsertAsync(TEntity entity);
